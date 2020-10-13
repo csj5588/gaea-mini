1820524079fe4be095c57fcdad54c7449538740b
cuishijie <cuishijie@inke.cn>
2020-10-13 14:47:19 +0800

    pages: category

--- src/pages/category/index.jsx
+++ src/pages/category/index.jsx
@@ -1,32 +1,56 @@
 import React, { Component } from 'react'
+import Taro from '@tarojs/taro'
 import { View, Text } from '@tarojs/components'
 import { AtButton } from 'taro-ui'
+import CardLine from '@/components/cardLine';
 import MenuCard from '@/components/menuCard';
 
 import "taro-ui/dist/style/components/button.scss" // 按需引入
 import './index.less'
 
 export default class Category extends Component {
+  state = {
+    data: [],
+  }
 
-  componentWillMount () { }
+  componentDidMount() {
+    this.getData()
+  }
 
-  componentDidMount () { }
+  getData = () => {
+    const db = wx.cloud.database();
+    const table = db.collection('category')
+    table.doc('category').get({
+      success: (res) => {
+        const { data } = res.data;
+        this.setState({ data });
+      }
+    });
+  }
 
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
+  routeTo = () => {
+    Taro.navigateTo({
+      url: '/pages/columns/index'
+    })
+  }
 
   render () {
+    const { data } = this.state;
     return (
-      <View className='at-row category'>
-        <View className='at-col card'>
-          <MenuCard />
-        </View>
-        <View className='at-col card'>
-          <MenuCard />
-        </View> 
+      <View className='category'>
+        {
+          data.map(item => {
+            return (
+              <View className='card' onClick={this.routeTo}>
+                <MenuCard
+                  title={item.title}
+                />
+              </View>
+            )
+          })
+        }
+        <CardLine />
+        <CardLine />
       </View>
     )
   }
