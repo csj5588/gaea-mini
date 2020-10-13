9c2e15f67962edc04ef1d067ad323c786be7e019
cuishijie <cuishijie@inke.cn>
2020-10-13 14:47:10 +0800

    components: menuCard

--- src/components/menuCard/index.jsx
+++ src/components/menuCard/index.jsx
@@ -4,13 +4,16 @@ import './index.less'
 
 export default class MenuCard extends Component {
   render () {
+    const {
+      title = '',
+    } = this.props;
     return (
       <View className="menu-card">
         <View className="icon">
           <Image className="image-icon" src="https://s1.ax1x.com/2020/10/12/0WD7fP.png" />
         </View>
         <Text className="title">
-          JavaScript
+          {title}
         </Text>
       </View>
     )
