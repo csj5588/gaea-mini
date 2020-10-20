8eb6659f6c17a305b8a00e947d6bbd3bbf8c0fbb
cuishijie <cuishijie@inke.cn>
2020-10-20 16:22:31 +0800

    feat: category iconSrc

--- src/components/menuCard/index.jsx
+++ src/components/menuCard/index.jsx
@@ -2,15 +2,18 @@ import React, { Component } from 'react'
 import { View, Text, Image } from '@tarojs/components'
 import './index.less'
 
+const DEFAULT_ICON_SRC = 'https://s1.ax1x.com/2020/10/12/0WD7fP.png'
+
 export default class MenuCard extends Component {
   render () {
     const {
       title = '',
+      iconSrc = DEFAULT_ICON_SRC,
     } = this.props;
     return (
       <View className="menu-card">
         <View className="icon">
-          <Image className="image-icon" src="https://s1.ax1x.com/2020/10/12/0WD7fP.png" />
+          <Image className="image-icon" src={iconSrc} />
         </View>
         <Text className="title">
           {title}
